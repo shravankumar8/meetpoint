4fa44d6dad59d8bcd37911194f672d5c80883950
shravankumar8 <kumashravan5@gmail.com>
2024-04-17 12:42:30 +0530

    modified the actions to only listen on backend processes

--- backend/index.js
+++ backend/index.js
@@ -138,7 +138,7 @@ io.on("connection", (socket) => {
 
 app.use(express.json());
 app.get("/max", async (req, res) => {
-  res.json({ users:users,message:"testing adwdsswedwew" });
+  res.json({ users:users,message:"testing adwdsswedwedfdsw" });
 });
 app.post("/signup", async (req, res) => {
   const { email, password } = req.body;
